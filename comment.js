5c0f9ca5e94eeea7e4806f8bd756cb78554a167c
Patrick Iyiakimo <iyiakimopatrick2002@gmail.com>
2024-09-03 18:21:01 +0100

    refactored my js file

--- comment.js
+++ comment.js
@@ -3,7 +3,6 @@ const commentsList = document.querySelector(".comments-list");
 const commentContainer = document.querySelector(".add-comment-container");
 let dataArray;
 
-// Fetch comments data
 fetch("data.json")
   .then((response) => response.json())
   .then((data) => {
@@ -11,170 +10,203 @@ fetch("data.json")
     displayComments();
   });
 
-// Event listener for adding a new comment
-commentContainer.querySelector("#send-comment-btn").addEventListener("click", addNewComment);
+commentContainer
+  .querySelector("#send-comment-btn")
+  .addEventListener("click", () => {
+    const newComment = commentContainer.querySelector("#comment-text").value;
+    commentContainer.querySelector("#comment-text").value = "";
+    dataArray.comments.push({
+      id: getLastId() + 1,
+      content: newComment,
+      createdAt: "now",
+      score: 0,
+      user: dataArray.currentUser,
+      replies: [],
+    });
+    displayComments();
+  });
 
-// Display comments
 function displayComments() {
   commentsList.innerHTML = "";
   dataArray.comments.forEach((comment) => {
-    const commentElement = createCommentElement(comment);
-    displayReplies(commentElement, comment.replies);
-    commentsList.appendChild(commentElement);
+    const newComment = createComment(comment);
+    comment.replies.forEach((reply) => {
+      const newReply = createComment(reply);
+      newReply.querySelector(
+        ".replying-to"
+      ).innerText = `@${reply.replyingTo} `;
+      newComment.querySelector(".replies-list").appendChild(newReply);
+    });
+    commentsList.appendChild(newComment);
   });
 }
 
-// Create a comment element
-function createCommentElement(comment) {
+function createComment(comment) {
   const newComment = commentTemplate.cloneNode(true);
   newComment.classList.remove("hidden");
-  fillCommentDetails(newComment, comment);
-  
-  if (isCurrentUser(comment.user)) {
-    setUpUserButtons(newComment, comment);
+  newComment.querySelector("header img").src = comment.user.image.png;
+  newComment.querySelector(".comment-name").innerText = comment.user.username;
+  newComment.querySelector(".comment-created-at").innerText = comment.createdAt;
+  newComment.querySelector(".comment-content .comment-text").innerText =
+    comment.content;
+  newComment.querySelector(".comment-score span").innerText = comment.score;
+
+  if (comment.user.username === dataArray.currentUser.username) {
+    setUpUserButtons(newComment, comment, dataArray);
   } else {
     setUpReplyButton(newComment, comment);
   }
 
-  setUpScoreButtons(newComment, comment);
+  newComment.querySelector("#add-score-btn").addEventListener("click", () => {
+    comment.score++;
+    newComment.querySelector(".comment-score span").innerText = comment.score;
+  });
+  newComment.querySelector("#sub-score-btn").addEventListener("click", () => {
+    if (comment.score == 0) return;
+    comment.score--;
+    newComment.querySelector(".comment-score span").innerText = comment.score;
+  });
 
   return newComment;
 }
 
-// Fill comment details
-function fillCommentDetails(commentElement, comment) {
-  commentElement.querySelector("header img").src = comment.user.image.png;
-  commentElement.querySelector(".comment-name").innerText = comment.user.username;
-  commentElement.querySelector(".comment-created-at").innerText = comment.createdAt;
-  commentElement.querySelector(".comment-content .comment-text").innerText = comment.content;
-  commentElement.querySelector(".comment-score span").innerText = comment.score;
-}
-
-// Display replies
-function displayReplies(commentElement, replies) {
-  const repliesList = commentElement.querySelector(".replies-list");
-  replies.forEach((reply) => {
-    const replyElement = createCommentElement(reply);
-    replyElement.querySelector(".replying-to").innerText = `@${reply.replyingTo} `;
-    repliesList.appendChild(replyElement);
-  });
-}
-
-// Set up score buttons
-function setUpScoreButtons(commentElement, comment) {
-  commentElement.querySelector("#add-score-btn").addEventListener("click", () => updateScore(comment, 1));
-  commentElement.querySelector("#sub-score-btn").addEventListener("click", () => updateScore(comment, -1));
-}
-
-// Update comment score
-function updateScore(comment, delta) {
-  comment.score = Math.max(0, comment.score + delta);
-  displayComments();
-}
-
-// Set up user buttons (edit, delete)
 function setUpUserButtons(commentElement, comment) {
   commentElement.querySelector(".you-stamp").classList.remove("hidden");
   commentElement.querySelector("#edit-btn").classList.remove("hidden");
+  commentElement.querySelector("#edit-btn").addEventListener("click", () => {
+    commentElement
+      .querySelector(".comment-edit-placeholder")
+      .classList.remove("hidden");
+    commentElement
+      .querySelector(".comment-edit-placeholder")
+      .querySelector("textarea").value =
+      commentElement.querySelector(".comment-text").innerText;
+    commentElement.querySelector(".comment-content").classList.add("hidden");
+    commentElement
+      .querySelector(".comment-edit-placeholder")
+      .querySelector("#comment-edit-btn")
+      .addEventListener("click", () => {
+        editComment(
+          comment,
+          commentElement
+            .querySelector(".comment-edit-placeholder")
+            .querySelector("textarea").value
+        );
+      });
+  });
   commentElement.querySelector("#delete-btn").classList.remove("hidden");
-
-  commentElement.querySelector("#edit-btn").addEventListener("click", () => initiateEditComment(commentElement, comment));
-  commentElement.querySelector("#delete-btn").addEventListener("click", () => confirmDeleteComment(comment));
-}
-
-// Initiate comment editing
-function initiateEditComment(commentElement, comment) {
-  const editPlaceholder = commentElement.querySelector(".comment-edit-placeholder");
-  editPlaceholder.classList.remove("hidden");
-  editPlaceholder.querySelector("textarea").value = comment.content;
-  commentElement.querySelector(".comment-content").classList.add("hidden");
-
-  editPlaceholder.querySelector("#comment-edit-btn").addEventListener("click", () => {
-    editComment(comment, editPlaceholder.querySelector("textarea").value);
+  commentElement.querySelector("#delete-btn").addEventListener("click", () => {
+    const deleteConfirm = document.querySelector(".delete-confirm-container");
+    deleteConfirm.classList.remove("hidden");
+    deleteConfirm
+      .querySelector("#delete-cancel-btn")
+      .addEventListener("click", () => {
+        deleteConfirm.classList.add("hidden");
+      });
+    deleteConfirm
+      .querySelector("#delete-confirm-btn")
+      .addEventListener("click", () => {
+        deleteComment(comment);
+        deleteConfirm.classList.add("hidden");
+      });
   });
 }
 
-// Set up reply button
 function setUpReplyButton(commentElement, comment) {
-  const replyButton = commentElement.querySelector("#reply-btn");
-  replyButton.classList.remove("hidden");
-
-  replyButton.addEventListener("click", () => {
+  commentElement.querySelector("#reply-btn").classList.remove("hidden");
+  commentElement.querySelector("#reply-btn").addEventListener("click", () => {
     const replyContainer = commentElement.querySelector(".reply-container");
     replyContainer.classList.toggle("hidden");
-    replyContainer.querySelector("textarea").value = `@${comment.user.username}, `;
+    replyContainer.querySelector(
+      "textarea"
+    ).innerText = `@${comment.user.username}, `;
+    replyContainer
+      .querySelector("#comment-reply-btn")
+      .addEventListener("click", () => {
+        let replyContent = replyContainer.querySelector("textarea").value;
+        replyContent = replyContent.replace(`@${comment.user.username}, `, "");
+        replyComment(comment, replyContent);
+      });
+  });
+}
 
-    replyContainer.querySelector("#comment-reply-btn").addEventListener("click", () => {
-      const replyContent = replyContainer.querySelector("textarea").value.replace(`@${comment.user.username}, `, "");
-      replyToComment(comment, replyContent);
+function deleteComment(comment) {
+  dataArray.comments.forEach((dataComment, index) => {
+    if (dataComment.id === comment.id) {
+      dataArray.comments.splice(index, 1);
+      displayComments();
+      return;
+    }
+    dataComment.replies.forEach((dataReply, replyIndex) => {
+      if (dataReply.id === comment.id) {
+        dataComment.replies.splice(replyIndex, 1);
+        displayComments();
+        return;
+      }
     });
   });
 }
 
-// Add a new comment
-function addNewComment() {
-  const newCommentText = commentContainer.querySelector("#comment-text").value.trim();
-  if (newCommentText === "") return;
-
-  commentContainer.querySelector("#comment-text").value = "";
-  const newComment = {
-    id: getNextId(),
-    content: newCommentText,
-    createdAt: "now",
-    score: 0,
-    user: dataArray.currentUser,
-    replies: [],
-  };
-
-  dataArray.comments.push(newComment);
-  displayComments();
-}
-
-// Reply to a comment
-function replyToComment(comment, content) {
-  const reply = {
-    id: getNextId(),
-    content,
-    createdAt: "now",
-    score: 0,
-    replyingTo: comment.user.username,
-    user: dataArray.currentUser,
-  };
-
-  if (!comment.replies) {
-    comment.replies = [];
-  }
-
-  comment.replies.push(reply);
-  displayComments();
-}
-
-// Edit a comment
 function editComment(comment, content) {
-  comment.content = content;
-  comment.createdAt = "now";
-  displayComments();
-}
-
-// Delete a comment
-function deleteComment(comment) {
-  dataArray.comments = dataArray.comments.filter((dataComment) => {
-    if (dataComment.id === comment.id) return false;
-    dataComment.replies = dataComment.replies.filter((reply) => reply.id !== comment.id);
-    return true;
+  dataArray.comments.forEach((dataComment, index) => {
+    if (dataComment.id === comment.id) {
+      dataArray.comments[index].content = content;
+      displayComments();
+      return;
+    }
+    dataComment.replies.forEach((dataReply, replyIndex) => {
+      if (dataReply.id === comment.id) {
+        dataArray.comments[index].replies[replyIndex].content = content;
+        dataArray.comments[index].replies[replyIndex].createdAt = "now";
+        displayComments();
+        return;
+      }
+    });
   });
-  displayComments();
 }
 
-// Get the next ID for a new comment or reply
-function getNextId() {
-  const lastId = Math.max(
-    ...dataArray.comments.map((comment) => [comment.id, ...comment.replies.map((reply) => reply.id)]).flat()
-  );
-  return lastId + 1;
+function replyComment(comment, content) {
+  dataArray.comments.forEach((dataComment, index) => {
+    if (dataComment.id === comment.id) {
+      dataArray.comments[index].replies.push({
+        id: getLastId() + 1,
+        content: content,
+        createdAt: "now",
+        score: 0,
+        replyingTo: comment.user.username,
+        user: dataArray.currentUser,
+      });
+      displayComments();
+      return;
+    }
+    dataComment.replies.forEach((dataReply, replyIndex) => {
+      if (dataReply.id === comment.id) {
+        dataArray.comments[index].replies[replyIndex].replies.push({
+          id: getLastId() + 1,
+          content: content,
+          createdAt: "now",
+          score: 0,
+          replyingTo: comment.user.username,
+          user: dataArray.currentUser,
+        });
+        displayComments();
+        return;
+      }
+    });
+  });
 }
 
-// Check if the comment belongs to the current user
-function isCurrentUser(user) {
-  return user.username === dataArray.currentUser.username;
+function getLastId() {
+  let lastId = 0;
+  dataArray.comments.forEach((comment) => {
+    if (comment.id > lastId) {
+      lastId = comment.id;
+    }
+    comment.replies.forEach((relpy) => {
+      if (relpy.id > lastId) {
+        lastId = relpy.id;
+      }
+    });
+  });
+  return lastId;
 }
